b87e4ac6a7bc0e4d32b2a1a358b4c3dad8651532
Sidney Piper <sidlapip@gmail.com>
2024-12-14 02:26:39 +0100

    day 13 :)

--- bin/main.ml
+++ bin/main.ml
@@ -57,4 +57,7 @@ let () =
     | 12 -> 
       Printf.printf "Part 1: %s\n" (Advent_of_code_2024.Day12.solve_part1 input);
       Printf.printf "Part 2: %s\n" (Advent_of_code_2024.Day12.solve_part2 input);
+    | 13 -> 
+      Printf.printf "Part 1: %s\n" (Advent_of_code_2024.Day13.solve_part1 input);
+      Printf.printf "Part 2: %s\n" (Advent_of_code_2024.Day13.solve_part2 input);
     | _ -> Printf.printf "Day %d not implemented yet\n" day
